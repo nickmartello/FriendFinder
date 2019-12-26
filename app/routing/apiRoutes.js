6d7f9c4f4faf5f6d5f4ae1f7b0d79416c9942917
Nick Martello <thenickmartello@gmail.com>
2019-12-26 16:25:07 -0500

    changing routes

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,46 +1,41 @@
-// ===============================================================================
-// LOAD DATA
-// We are linking our routes to a series of "data" sources.
-// These data sources hold arrays of information on table-data, waitinglist, etc.
-// ===============================================================================
+var friends = require("../data/friends");
 
-var tableData = require("../data/friends");
+module.exports = function(app){
+    app.get("../data/friends.js", function(_req, res){
+        res.json(friends);
+    });
 
+    app.post("../data/friends.js", function(req, res){
+        var bestMatch = {
+            name: "",
+            friendDifference: Infinity
+        };
 
+        var userData = req.body;
+        var userScores = userData.scores;
 
-// ===============================================================================
-// ROUTING
-// ===============================================================================
+        var totalDifference;
 
-module.exports = function(app) {
-  // API GET Requests
-  // Below code handles when users "visit" a page.
-  // In each of the below cases when a user visits a link
-  // (ex: localhost:PORT/api/admin... they are shown a JSON of the data in the table)
-  // ---------------------------------------------------------------------------
+        for(var i = 0; i < friends.length; i++){
+            var currentFriend = friends[i];
+            totalDifference = 0;
 
-  app.get("/api/friends", function(req, res) {
-    res.json(tableData);
-  });
+            console.log(currentFriend.name);
 
-  // API POST Requests
-  // Below code handles when a user submits a form and thus submits data to the server.
-  // In each of the below cases, when a user submits form data (a JSON object)
-  // ...the JSON is pushed to the appropriate JavaScript array
-  // (ex. User fills out a reservation request... this data is then sent to the server...
-  // Then the server saves the data to the tableData array)
-  // ---------------------------------------------------------------------------
+            for(var j = 0; j < currentFriend.scores.length; j++){
+                var currentFriendScore = currentFriend.scores[j];
+                var currentUserScore = userScores[j];
 
-  app.post("/api/friends", function(req, res) {
-    
-  });
+                totalDifference += Math.abs(parseInt(currentUserScore) - parseInt(currentFriendScore));
+            }
+            if (totalDifference <= bestMatch.friendDifference){
+                bestMatch.name = currentFriend.name;
+              
+                bestMatch.friendDifference = totalDifference;
+            }
+        }
+        friends.push(userData);
 
-
-  app.post("/api/clear", function(req, res) {
-    // Empty out the arrays of data
-    tableData.length = 0;
-    waitListData.length = 0;
-
-    res.json({ ok: true });
-  });
+        res.json(bestMatch);
+    });
 };
--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -8,20 +8,13 @@ var path = require("path");
 // ===============================================================================
 // ROUTING
 // ===============================================================================
+var path = require("path");
 
-module.exports = function(app) {
-  // HTML GET Requests
-  // Below code handles when users "visit" a page.
-  // In each of the below cases the user is shown an HTML page of content
-  // ---------------------------------------------------------------------------
-
-  app.get("/survey", function(req, res) {
-    res.sendFile(path.join(__dirname, "./public/survey.html"));
-  });
-
-  
-  // If no matching route is found default to home
-  app.get("*", function(req, res) {
-    res.sendFile(path.join(__dirname, "./public/home.html"));
-  });
+module.exports = function(app){
+    app.get("/survey", function(_req, res){
+        res.sendFile(path.join(__dirname, "../public/survey.html"));
+    });
+    app.get("*", function(_req, res){
+        res.sendFile(path.join(__dirname, "../public/home.html"));
+    });
 };
